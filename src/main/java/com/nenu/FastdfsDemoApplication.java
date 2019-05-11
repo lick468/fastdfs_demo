75e3de2770d68a8c3f3ca8210e56df9849fc086d
lick468 <987723764@qq.com>
2019-05-11 18:14:14 +0800

    init

---
+++ src/main/java/com/nenu/FastdfsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastdfsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastdfsDemoApplication.class, args);
+    }
+
+}
